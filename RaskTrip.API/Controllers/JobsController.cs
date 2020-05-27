486b52311d40d8a10b17bfb65bc70ce317d9c05a
mneed <mneed@adaptivesys.com>
2020-05-27 12:47:56 -0400

    Clock-out, first round. Need to ask Dave P about PropertyFlatRates being needed in here.

--- RaskTrip.API/Controllers/JobsController.cs
+++ RaskTrip.API/Controllers/JobsController.cs
@@ -144,6 +144,7 @@ namespace RaskTrip.API.Controllers
 		public IHttpActionResult PostClockIn([FromBody] ClockInDto clockIn)
 		{
 			// TODO: verify basic authentication from http header (write a common method to do this)
+
 			var job = db.Jobs.Include("TripRoute.Trip").FirstOrDefault(j => j.JobId.Equals(clockIn.JobId));
 						
 			if (job != null
@@ -177,21 +178,61 @@ namespace RaskTrip.API.Controllers
 			return NotFound();
 		}
 
-		//[HttpPost]
-		//public void PostClockOut(StringContent jobData)
-		//{
-		//	var clockOutTime = DateTime.Now;
-		//	// Work with team/Dave P on data points
-		// TODO: I would create a smaller, ClockOutDto that has only the properties necessary to complete this transaction...
-		// TODO: verify basic authentication from http header (write a common method to do this)
-		// TODO: get the job from the database identified by the jobDto.JobId  -- include TripRoute and Trip
-		// TODO: verify that it's OK to update the job: if job.TripStatusId == TripStatusEnum.InProcess.GetHashCode() and the job is for this truckId
-		// TODO: Find the PropertyFlatRate corresponding to the JobPropertyFlatRateId.  Lookup the "sibling" if the Dto's JobServiceName is different
-		// TODO: determine which TripStatusId to use: SkippedNotPlowed, SkippedNoAccess, or Completed
-		// TODO: update the following job properties:
-		// TODO: ActualClockOut, ActualWeightOut and compute ActualTonnage (ifJobRequiresWeighInOut), ActualPropertyFlatRateId, TripStatusId
-		// TODO: save the job.
-		//}
+		[HttpPost]
+		public IHttpActionResult PostClockOut([FromBody] ClockOutDto clockOut)
+		{
+			// TODO: verify basic authentication from http header (write a common method to do this)
+
+			var clockOutTime = DateTime.Now;
+			var job = db.Jobs.Include("TripRoute.Trip").FirstOrDefault(j => j.JobId.Equals(clockOut.JobId));
+
+			if (job != null
+				&& job.TripRoute.TripStatusId == Enums.TripStatusEnum.InProcess.GetHashCode()
+				&& job.TripStatusId == Enums.TripStatusEnum.InProcess.GetHashCode())
+			{
+				if (job.JobId == clockOut.JobId)
+				{
+					try
+					{
+						job.ActualClockOut = DateTime.Now;
+						// TODO: Find the PropertyFlatRate corresponding to the JobPropertyFlatRateId.  Lookup the "sibling" if the Dto's JobServiceName is different
+						// TODO: Why do I need this to clock out? ActualPropertyFlatRateId.... 
+						
+						if (clockOut.ActualServicePerformed == Enums.TripStatusEnum.SkippedNotPlowed.ToString())
+						{
+							job.TripRoute.TripStatusId = Enums.TripStatusEnum.SkippedNotPlowed.GetHashCode();
+							job.TripStatusId = Enums.TripStatusEnum.SkippedNotPlowed.GetHashCode();
+						}
+						else if (clockOut.ActualServicePerformed == Enums.TripStatusEnum.SkippedNoAccess.ToString())
+						{
+							job.TripRoute.TripStatusId = Enums.TripStatusEnum.SkippedNoAccess.GetHashCode();
+							job.TripStatusId = Enums.TripStatusEnum.SkippedNoAccess.GetHashCode();
+						}
+						else if (clockOut.ActualServicePerformed == Enums.TripStatusEnum.Completed.ToString())
+						{
+							job.TripRoute.TripStatusId = Enums.TripStatusEnum.Completed.GetHashCode();
+							job.TripStatusId = Enums.TripStatusEnum.Completed.GetHashCode();
+						}
+						
+						if (job.JobRequiresWeighInOut)
+						{
+							job.ActualWeightOut = clockOut.ActualWeightOut;
+							job.ActualTonnage = job.ActualWeightIn = clockOut.ActualWeightOut;
+						}
+
+						db.SaveChanges();
+						return Ok(clockOut);
+					}
+					catch (Exception ex)
+					{
+						//Logger ex.????;
+						return NotFound();
+					}
+				}
+			}
+			return NotFound();
+		}
+
 
 		/// <summary>
 		/// Verifies the basic authentication credentials passed in the HttpRequest header. The format of the basic auth is illustrated by the 
