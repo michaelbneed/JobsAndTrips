b859552ff430dc073c4dbfcf7454fa647b821966
DAVID PECENKA <dpecenka@adaptivesys.com>
2020-06-19 17:18:01 -0400

    reinstating commented out credential checking code

--- RaskTrip/RaskTrip/App.xaml.cs
+++ RaskTrip/RaskTrip/App.xaml.cs
@@ -18,20 +18,19 @@ namespace RaskTrip
 
 		protected override void OnStart()
 		{
-			//MainPage.Navigation.PushAsync(new DirectionsPage());
-			//var truckCredentials = CredentialsManager.GetLoginCredentials();
-			//if (truckCredentials.TruckId > 0)
-			//{
-			//	var validCredentials = CredentialsManager.VerifyCredentials(truckCredentials);
-			//	if (validCredentials.TruckId > 0)
-			//		MainPage.Navigation.PushAsync(new DirectionsPage());
-			//	else
-			//		MainPage.Navigation.PushAsync(new RegisterLoginPage());
-			//}
-			//else
-			//{
-			//	MainPage.Navigation.PushAsync(new RegisterLoginPage());
-			//}
+			var truckCredentials = CredentialsManager.GetLoginCredentials();
+			if (truckCredentials.TruckId > 0)
+			{
+				var validCredentials = CredentialsManager.VerifyCredentials(truckCredentials);
+				if (validCredentials.TruckId > 0)
+					MainPage.Navigation.PushAsync(new DirectionsPage());
+				else
+					MainPage.Navigation.PushAsync(new RegisterLoginPage());
+			}
+			else
+			{
+				MainPage.Navigation.PushAsync(new RegisterLoginPage());
+			}
 		}
 
 		protected override void OnSleep()
